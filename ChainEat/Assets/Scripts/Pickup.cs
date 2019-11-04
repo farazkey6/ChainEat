d9bb77efc0ee5ac7779f1e4300cc01207fe32459
AntTheAntster <anttheantster@gmail.com>
2019-11-04 14:21:48 +0000

    Basic Health, Throwing, Dropping and Death
    E to pickup
    Left click to throw
    Right click to drop
    
    enemy will destroy on health being 0

--- ChainEat/Assets/Scripts/Pickup.cs
+++ ChainEat/Assets/Scripts/Pickup.cs
@@ -45,24 +45,67 @@ public class Pickup : MonoBehaviour
             item.GetComponent<Rigidbody2D>().gravityScale = 1;
             item.transform.position = objectPos;
         }
+        PickupObject();
+        ThrowObject();
+        
     }
 
     // If object is clicked
-    void OnMouseDown()
+    void PickupObject()
     {
-        // Check if the player is in range of object
-        if(distance <= 1f)
+        if (Input.GetKey("e"))
         {
-            isHolding = true;
-            item.GetComponent<Rigidbody2D>().gravityScale = 0;
-            item.GetComponent<Rigidbody2D>().collisionDetectionMode = 0;
-        }
-        
+            // Check if the player is in range of object
+            if (distance <= 1f)
+            {
+                // When picked up
+                if (isHolding == false)
+                {
+                    isHolding = true;
+                    item.GetComponent<Rigidbody2D>().gravityScale = 0;
+                    item.GetComponent<Rigidbody2D>().collisionDetectionMode = 0;
+                }
 
+            }
+        }
     }
-    void OnMouseUp()
+    void ThrowObject()
     {
-        isHolding = false;
+        // Chck if the player is holding an object
+        if (isHolding == true)
+        {
+            // Throw with Left Click
+            if (Input.GetMouseButton(0))
+            {
+                // Add force based on the value of "throwForce"
+                item.GetComponent<Rigidbody2D>().AddForce(Vector2.right * throwForce);
+                isHolding = false;
+            }
+            else
+            {
+                // If isHolding = False, return nothing
+                return;
+            }
+        }
+    }
+    void DropObject()
+    {
+        if (isHolding == true)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                isHolding = false;
+            }
+        }
+    }
+
+    // If rock collides with enemy
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
