5b09e1b8de881df2561941adcacd4a1e06ab109c
AntTheAntster <anttheantster@gmail.com>
2019-11-07 21:05:01 +0000

    Some... Testing on and off topic
    * Overhauled item pickup registration
    * changed the enemy controller a bit
    * layed foundations for enemies to use Rocks n such
    * messed around a bit with ragdolls
    *had fun

---
+++ ChainEat/Assets/Test/Test.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+
+    public GameObject leftLeg;
+    public GameObject rightLeg;
+
+    public bool grounded;
+    public bool canJump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+        if (leftLeg.gameObject.GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")) || rightLeg.gameObject.GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            grounded = true;
+        } else
+        {
+            grounded = false;
+            canJump = false;
+        }
+        if (grounded == true)
+        {
+            leftLeg.gameObject.GetComponent<Rigidbody2D>().mass = 1;
+            rightLeg.gameObject.GetComponent<Rigidbody2D>().mass = 1;
+            canJump = true;
+        }
+        if (grounded == false)
+        {
+            leftLeg.gameObject.GetComponent<Rigidbody2D>().mass = 5;
+            rightLeg.gameObject.GetComponent<Rigidbody2D>().mass = 5;
+        }
+    }
+
+    private void Moving()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.GetComponent<Rigidbody2D>().AddForce(Vector2.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (canJump == true && grounded == true)
+            {
+                Jump(jumpForce);
+                canJump = false;
+                grounded = false;
+            }
+            
+        }
+    }
+
+    private void Jump(float f)
+    {
+        
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, f), ForceMode2D.Impulse);
+
+        canJump = false;
+        grounded = false;
+    }
+    
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
